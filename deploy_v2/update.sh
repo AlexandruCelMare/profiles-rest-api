516d33eca3d2d77cc732d9990a7bcf339ca2cc3c
Alexander Butarita <alexander.butarita.business@gmail.com>
2022-12-27 00:49:09 -0800

    Error fix text 1.5

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -5,8 +5,8 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api/profiles-rest-api'
 
 git pull
-manage.py migrate
-manage.py collectstatic --noinput
+$PROJECT_BASE_PATH/usr/local/apps/profiles-rest-api/profiles-rest-api manage.py migrate
+$PROJECT_BASE_PATH/usr/local/apps/profiles-rest-api/profiles-rest-api manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
